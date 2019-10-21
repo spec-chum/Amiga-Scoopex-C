b6751c79e9aa230b1b86fc6b399223104392035e
Spec-Chum <specchumster@gmail.com>
2019-10-21 01:03:24 +0100

    Complete tut8

---
+++ tut8/support/gcc8_c_support.h
@@ -0,0 +1,37 @@
+#pragma once
+void *memcpy (void *, const void *, int);
+void *memmove (void *, const void *, int);
+void *memset (void *, int, int);
+int strlen (const char *);
+void warpmode(int on); // bool on/off
+void KPrintF(const char* fmt, ...); // output to debugger
+
+#define INCBIN(name, file) \
+    __asm__(".pushsection .rodata\n" \
+            ".global incbin_" #name "_start\n" \
+            ".type incbin_" #name "_start, @object\n" \
+            ".balign 2\n" \
+            "incbin_" #name "_start:\n" \
+            ".incbin \"" file "\"\n" \
+            \
+            ".global incbin_" #name "_end\n" \
+            ".type incbin_" #name "_end, @object\n" \
+            ".balign 1\n" \
+            "incbin_" #name "_end:\n" \
+            ".byte 0\n" \
+			".popsection\n" \
+    ); \
+    extern const __attribute__((aligned(2))) char incbin_ ## name ## _start[1024*1024]; \
+	extern const void* incbin_ ## name ## _end;\
+    const void* name = &incbin_ ## name ## _start;
+
+inline unsigned int muluw(unsigned short b,unsigned short c) {
+    unsigned short a;
+    asm("muluw %2,%0":"=d"(a): "0"(c),"d"(b): "cc");
+    return a;
+}
+inline int mulsw(short b,short c) {
+    int a;
+    asm("mulsw %2,%0":"=d"(a): "0"(c),"d"(b): "cc");
+    return a;
+}
---
+++ tut8/tut8.c
@@ -0,0 +1,138 @@
+/*	Understanding the Memory
+
+	Tutorial 7 of Scoopex's incredible Amiga Hardware Programming Series on YouTube
+
+	https://www.youtube.com/watch?v=ZPJW3wIfL4I&list=PLc3ltHgmiidpK-s0eP5hTKJnjdTHz0_bW&index=7
+*/
+
+#include "support/gcc8_c_support.h"
+
+#include <proto/exec.h>
+#include <proto/graphics.h>
+#include <hardware/cia.h>
+#include <hardware/custom.h>
+#include <graphics/gfxbase.h>
+
+#include "../headers/custom_defines.h"
+#include "../headers/helpers.h"
+
+#define WAITRAS1 28
+
+struct ExecBase *SysBase;
+struct GfxBase *GfxBase;
+struct copinit *oldCopinit;
+
+volatile struct Custom *custom = (struct Custom *)0xdff000;
+volatile struct CIA *ciaa  = (struct CIA *)0xbfe001;
+
+/*	__attribute__((section("tut.MEMF_CHIP")))	
+	seems to set the section as readonly	*/
+UWORD copperlist[] =
+{
+	CMOVE(FMODE, 0x000),		// set FMODE to slow for AGA
+	CMOVE(BPLCON0, 0x200),		// no bitplanes, but need color burst
+
+	CMOVE(COLOR00, 0x349),
+	CWAIT(0x2b07, 0xfffe),
+	CMOVE(COLOR00, 0x56c),
+	CWAIT(0x2c07, 0xfffe),
+	CMOVE(COLOR00, 0x113),
+
+// waitras1:
+	CWAIT(0x8007, 0xfffe),
+	CMOVE(COLOR00, 0x055),
+
+// waitras2:
+	CWAIT(0x8107, 0xfffe),
+	CMOVE(COLOR00, 0x0aa),
+
+// waitras3:
+	CWAIT(0x8207, 0xfffe),
+	CMOVE(COLOR00, 0x0ff),
+
+// waitras4:
+	CWAIT(0x8307, 0xfffe),
+	CMOVE(COLOR00, 0x0aa),
+
+// waitras5:
+	CWAIT(0x8407, 0xfffe),
+	CMOVE(COLOR00, 0x055),
+
+// waitras6:
+	CWAIT(0x8507, 0xfffe),
+	CMOVE(COLOR00, 0x113),
+
+	CWAIT(0xffdf, 0xfffe),
+	CWAIT(0x2c07, 0xfffe),
+	CMOVE(COLOR00, 0x56c),
+	CWAIT(0x2d07, 0xfffe),
+	CMOVE(COLOR00, 0x349),
+
+	CEND		
+};
+
+int main()
+{
+	SysBase = *(struct ExecBase**)4L;
+
+	// place our copperlist in chipmem
+	UBYTE *clptr = AllocMem(sizeof(copperlist), MEMF_CHIP);
+	CopyMem(copperlist, clptr, sizeof(copperlist));
+
+	// open gfx lib and save original copperlist
+	GfxBase = (struct GfxBase*)OldOpenLibrary("graphics.library");
+	oldCopinit = GfxBase->copinit;
+	CloseLibrary((struct Library *)GfxBase);
+	
+	// line starting position
+	UWORD yPos = 0xac;
+
+	// line direction
+	WORD yDir = 1;
+
+	// Save interrupts and DMA
+	UWORD oldInt = custom->intenar;
+
+	// disable all interrupts
+	custom->intena = 0x7fff;
+
+	// initiate our copper
+	custom->cop1lc = (ULONG)clptr;
+
+	// loop until mouse clicked
+	while(ciaa->ciapra & CIAF_GAMEPORT0)
+	{
+		// wait for start of frame ** wframe **
+		if((custom->vposr & 1) == 0 && HIBYTE(custom->vhposr) == 0x2a)
+		{
+			while(HIBYTE(custom->vhposr) == 0x2a)
+			{ } // wframe2
+			
+			// update line y position
+			yPos += yDir;
+
+			// bounce line
+			if(yPos > 0xf0 || yPos < 0x40)
+			{
+				yDir = -yDir;
+			}
+
+			// dynamically update all waitras values
+			for(UWORD waitras = 0; waitras < 6; waitras++)
+			{
+				clptr[WAITRAS1 + (waitras * 8)] = yPos + waitras;
+			}
+		}
+	}
+
+	// restore original copper
+	custom->cop1lc = (ULONG)oldCopinit;
+
+	// free copperlist memory
+	FreeMem(clptr, sizeof(copperlist));
+
+	// restore interrupts
+	custom->intena = oldInt | 0xc000;
+
+	return 0;
+}
